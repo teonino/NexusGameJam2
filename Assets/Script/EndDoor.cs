fc5da017722da9068850e2afec952546d6da11b7
VArnaud <107640450+ArnaudVdps@users.noreply.github.com>
2022-11-26 20:03:09 +0100

    Victory Screen

--- Assets/Script/EndDoor.cs
+++ Assets/Script/EndDoor.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndDoor : MonoBehaviour
 {
     [SerializeField] Animator BlackscreenAnimator;
     [SerializeField] Animator TextAnimator;
+    [SerializeField] List<Button> ButtonList;
+    [SerializeField] List<Animator> buttonAnimator;
+    [SerializeField] public MoonManagement SkillsManagement;
+
+    private void Start()
+    {
+        for(int i = 0; i < ButtonList.Count; i++)
+        {
+            ButtonList[i].interactable = false;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
@@ -22,6 +35,18 @@ public class EndDoor : MonoBehaviour
         BlackscreenAnimator.SetTrigger("TriggerFade");
         yield return new WaitForSeconds(1.5f);
         TextAnimator.SetTrigger("TriggerFade");
+        yield return new WaitForSeconds(0.5f);
+        for (int i3 = 0; i3 < ButtonList.Count; i3++)
+        {
+            ButtonList[i3].interactable = true;
+            for(int i2 = 0; i2 < buttonAnimator.Count; i2++)
+            {
+                yield return new WaitForSeconds(0.3f);
+                buttonAnimator[i2].SetTrigger("TriggerFade");
+            }
+
+        }
+        SkillsManagement.DisableAllSkills();
     }
 
 }
