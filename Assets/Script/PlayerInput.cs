6a89f2be83ff65ff3dbadd2979adca400238612e
VArnaud <107640450+ArnaudVdps@users.noreply.github.com>
2022-11-26 14:17:11 +0100

    update prefab + script separation

--- Assets/Script/PlayerInput.cs
+++ Assets/Script/PlayerInput.cs
@@ -9,19 +9,10 @@ public class PlayerInput : MonoBehaviour
     public KeyCode Backward;
     public KeyCode Left;
     public KeyCode Right;
-    [Header("Mechanics Movements")]
-    public KeyCode Jump;
-    public KeyCode slide;
-    public KeyCode dash;
+
     [Header("Player Characteristics")]
     [SerializeField] private float speed;
-    [SerializeField] private float JumpForce;
-    [SerializeField] private float JumpPoints;
-    [SerializeField] private float DashForce;
-    [SerializeField] private float DashCooldown;
     [SerializeField] internal bool HaveBoots;
-
-    private Vector3 MoveDir;
     private int hp = 1;
 
     [Header("Components")]
@@ -29,10 +20,9 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private GameObject Player;
     [SerializeField] private Collider BoxCollision;
     [SerializeField] private List<bool> DirectionBool;
-    internal bool DashIsAvaiable = false;
     private Vector3 direction;
     private Quaternion playerRotation;
-    [SerializeField] bool isGrounded = false;
+
 
     private void Update()
     {
@@ -59,50 +49,8 @@ public class PlayerInput : MonoBehaviour
         }
 
 
-        if (Input.GetKeyDown(Jump))
-        {
-            if (JumpPoints == 2 && isGrounded)
-            {
-                print("regular jump");
-                rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
-                JumpPoints -= 1;
-                isGrounded = false;
-            }
-            if (JumpPoints == 1)
-            {
-                print("Double jump");
-                rb.AddForce(Vector3.up * (JumpForce-2), ForceMode.Impulse);
-                JumpPoints -= 1;
-                isGrounded = false;
-            }
-            else
-            {
-                print("no more jump");
-            }
-            print(JumpPoints);
-        }
-        RaycastHit hit;
-        if (Physics.Raycast(Player.transform.position, Vector3.down, out hit, 1.4f))
-        {
-            JumpPoints = 2;
-            isGrounded = true;
-        }
-
-        if(Input.GetKeyDown(dash))
-        {
-            rb.AddForce(Player.transform.forward * DashForce, ForceMode.Impulse);
-            StartCoroutine(DashCooldownCoroutine());
-            DashIsAvaiable = false;
-
-        }
-
-
     }
 
-    private IEnumerator DashCooldownCoroutine()
-    {
-        yield return new WaitForSeconds(DashCooldown);
-        DashIsAvaiable = true;
-    }
+
 
 }
