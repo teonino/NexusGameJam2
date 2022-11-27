042136aee36e703760e1f4adb256e512f3e58a9c
VArnaud <107640450+ArnaudVdps@users.noreply.github.com>
2022-11-27 15:33:45 +0100

    Main Menu + Music Gestion

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,12 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject OptionsPanel;
+    public GameObject HowToPanel;
+    public GameObject CreditsPanel;
+    public GameObject MainPanel;
+    public AudioSource MainMusic;
+    public Slider SliderMusic;
+    public Music musicScript;
+
+    private void Start()
+    {
+        AudioListener.volume = SliderMusic.value;
+        musicScript.MusicVolume = AudioListener.volume;
+    }
+
+    public void DisplayOptions()
+    {
+        OptionsPanel.SetActive(true);
+        HowToPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        MainPanel.SetActive(false);
+    }
+    public void DisplayHowToPlay()
+    {
+        OptionsPanel.SetActive(false);
+        HowToPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        MainPanel.SetActive(false);
+    }
+
+    public void DisplayCredits()
+    {
+        OptionsPanel.SetActive(false);
+        HowToPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+        MainPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void DisplayMainMenu()
+    {
+        OptionsPanel.SetActive(false);
+        HowToPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        MainPanel.SetActive(true);
+    }
+
+
     public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
+
+    public void OnValueChanged()
+    {
+        AudioListener.volume = SliderMusic.value;
+        musicScript.MusicVolume = AudioListener.volume;
+    }
+
 }
---
+++ Assets/Script/Music.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public float MusicVolume;
+    [SerializeField] AudioClip[] music;
+    private AudioSource AudioMenu;
+    public int AudioToPlay = 0;
+
+ 
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        AudioMenu = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (!AudioMenu.isPlaying)
+        {
+            ChangeMusic();
+        }
+    }
+
+    public void ChangeMusic()
+    {
+        if (AudioToPlay == 2)
+        {
+            AudioMenu.clip = music[AudioToPlay];
+            AudioMenu.Play();
+            AudioToPlay = 0;
+        }
+        else
+        {
+            AudioMenu.clip = music[AudioToPlay];
+            AudioMenu.Play();
+            AudioToPlay += 1;
+        }
+    }
+
+
+}
